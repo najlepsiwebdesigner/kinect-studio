a4530bb80148475326df8ed8f1906a009c164c89
Peter Beno <najlepsiwebdesigner@gmail.com>
2018-04-11 17:27:31 +0200

    commit before TUM dataset compatibility implementation

--- src/MapModel.h
+++ src/MapModel.h
@@ -50,7 +50,6 @@ public:
 				// raise counter on this point hit counter
 				indices_hit_count[current_model_index]++;
 
-
 				// // average of current measurement and previous value
 				// feature_cloud->points[current_model_index].x = (feature_cloud->points[current_model_index].x + frame.feature_cloud->points[i].x)/2;
 				// feature_cloud->points[current_model_index].y = (feature_cloud->points[current_model_index].y + frame.feature_cloud->points[i].y)/2;
@@ -61,22 +60,21 @@ public:
 				// feature_cloud->points[current_model_index].y = frame.feature_cloud->points[i].y;
 				// feature_cloud->points[current_model_index].z = frame.feature_cloud->points[i].z;
 
-// running average
-feature_cloud->points[current_model_index].x = 
-	(feature_cloud->points[current_model_index].x * (indices_hit_count[current_model_index] -1) 
-	+ frame.feature_cloud->points[i].x) 
-	/ indices_hit_count[current_model_index];
+				// running average of feature position
+				feature_cloud->points[current_model_index].x = 
+					(feature_cloud->points[current_model_index].x * (indices_hit_count[current_model_index] -1) 
+					+ frame.feature_cloud->points[i].x) 
+					/ indices_hit_count[current_model_index];
 
-feature_cloud->points[current_model_index].y = 
-	(feature_cloud->points[current_model_index].y * (indices_hit_count[current_model_index] -1) 
-	+ frame.feature_cloud->points[i].y) 
-	/ indices_hit_count[current_model_index];
-	
-feature_cloud->points[current_model_index].z = 
-	(feature_cloud->points[current_model_index].z * (indices_hit_count[current_model_index] -1) 
-	+ frame.feature_cloud->points[i].z) 
-	/ indices_hit_count[current_model_index];
-
+				feature_cloud->points[current_model_index].y = 
+					(feature_cloud->points[current_model_index].y * (indices_hit_count[current_model_index] -1) 
+					+ frame.feature_cloud->points[i].y) 
+					/ indices_hit_count[current_model_index];
+					
+				feature_cloud->points[current_model_index].z = 
+					(feature_cloud->points[current_model_index].z * (indices_hit_count[current_model_index] -1) 
+					+ frame.feature_cloud->points[i].z) 
+					/ indices_hit_count[current_model_index];
 
 				descriptors.row(current_model_index) = frame.descriptors.row(i);
 				keypoints[current_model_index] = frame.keypoints[i];
@@ -103,6 +101,32 @@ feature_cloud->points[current_model_index].z =
 
 	Frame getPredictedFrame() {
 
+		// statistics about multi features
+		int multihit = 0;
+		int more_than_10 = 0;
+
+		for (auto & hit_count : indices_hit_count) {
+			if (hit_count != 0) {
+				multihit++;
+
+				if (hit_count > 10) {
+					more_than_10++;
+				}
+			}
+		}
+		std::cout 
+			<< "Percentage of multihit: " 
+			<< (multihit*100) / indices_hit_count.size() 
+			<< "% of indices total: " 
+			<< indices_hit_count.size() 
+			<< " more than 10: " 
+			<< more_than_10
+			<< std::endl;
+
+
+
+
+
 		Frame predicted_frame;
 		std::vector<int> feature_point_indices;
 
@@ -121,7 +145,7 @@ feature_cloud->points[current_model_index].z =
 				camera_poses_to_indices[i].end()
 			);
 		}
-		// std::cout << feature_point_indices.size() << std::endl;
+		
 
 		// std::cout << "camera_poses_size: " << camera_poses.size() << std::endl;
 		// std::cout << "feature_cloud_size: " << feature_cloud->points.size() << std::endl;
@@ -137,9 +161,10 @@ feature_cloud->points[current_model_index].z =
 		// std::cout << "multi hit indices count: " << good_indices_counter << std::endl;
 		// bool only_multi_features = good_indices_counter > 0;
 
-
-// sort( feature_point_indices.begin(), feature_point_indices.end() );
-// feature_point_indices.erase( unique( feature_point_indices.begin(), feature_point_indices.end() ), feature_point_indices.end() );
+std::cout << "feature point in predicted frame count: " <<  feature_point_indices.size() << std::endl;
+sort( feature_point_indices.begin(), feature_point_indices.end() );
+feature_point_indices.erase( unique( feature_point_indices.begin(), feature_point_indices.end() ), feature_point_indices.end() );
+std::cout << "feature point in predicted frame  after reduction count: " <<  feature_point_indices.size() << std::endl;
 
 		for (int i = 0; i < feature_point_indices.size(); i++) {
 			// if (only_multi_features && indices_hit_count[feature_point_indices[i]] < 1) continue; 
